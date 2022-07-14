e275ae9b67dd77ce3634385878741b501af3def0
kolarius27 <michalkolar27@gmail.com>
2022-07-14 15:31:46 +0200

    soup added, creating polygons

--- annotating/api_wrapper.py
+++ annotating/api_wrapper.py
@@ -3,29 +3,17 @@ import json
 import requests
 from osgeo import ogr
 import re
-
-URL = "https://www.geoportalpraha.cz/cs/data/otevrena-data/468E977C-DE78-480D-B3D9-43A19BF1CD77"
-#URL_jgw = "https://opendata.iprpraha.cz/CUR/ORT/ORT_CIR/S_JTSK/{identifier}.jgw"
-URL_jgw = "https://opendata.iprpraha.cz/CUR/ORT/ORT_CIR/S_JTSK/Bero_0-0-11.jgw"
-URL_jpg = "https://opendata.iprpraha.cz/CUR/ORT/ORT_CIR/S_JTSK/{identifier}.jpg"
+from bs4 import BeautifulSoup
+import geopandas as gpd
+from shapely.geometry import Polygon
+import numpy as np
 
 
 def envelope_to_polygon(minX, maxX, minY, maxY):
-    # Create ring
-    ring = ogr.Geometry(ogr.wkbLinearRing)
-    ring.AddPoint(minX, minY)
-    ring.AddPoint(maxX, minY)
-    ring.AddPoint(maxX, maxY)
-    ring.AddPoint(minX, maxY)
-    ring.AddPoint(minX, minY)
-
-    # Create polygon
-    poly_envelope = ogr.Geometry(ogr.wkbPolygon)
-    poly_envelope.AddGeometry(ring)
-    return poly_envelope
+    return Polygon([(minX, minY), (maxX, minY), (maxX, maxY), (minX, maxY)])
 
 
-def get_json(url):
+def get_jgw(url):
     r = requests.get(url)
     if r.status_code != 200:
         raise RuntimeError(r.text)
@@ -44,13 +32,32 @@ def jgw_to_envelope(lines):
     return minX, maxX, minY, maxY
 
 
+def get_polygons_from_url(url_main, url_jgw, poly_path):
+    if not os.path.exists(poly_path):
+        main_html = requests.get(url_main)
+        content = main_html.text
+        soup = BeautifulSoup(content)
+        polygons = []
+        filenames = []
+        for div in soup.findAll('div', attrs={'class': 'col-lg-4 col-md-6'})[1:]:
+            filename = div.find('strong').get_text()
+            filenames.append(filename)
+            jgw = get_jgw(url_jgw.format(identifier=filename))
+            minX, maxX, minY, maxY = jgw_to_envelope(jgw)
+            polygons.append(envelope_to_polygon(minX, maxX, minY, maxY))
+        grid = gpd.GeoDataFrame({'name': filenames, 'geometry': polygons})
+        grid.to_file(poly_path)
+    else:
+        print(f"  grid {poly_path} already created")
+
+
 def get_features_from_bbox(bbox):
     """Return all features for bounding box"""
 
     url = URL.format(bbox=",".join(str(f) for f in bbox))
     features = []
     while True:
-        r_json = get_json(url)
+        r_json = get_jgw(url)
         features += r_json["features"]
         more_features = False
         for link in r_json['links']:
@@ -66,3 +73,12 @@ def get_features_from_bbox(bbox):
 
 
 if __name__ == '__main__':
+    URL = "https://www.geoportalpraha.cz/cs/data/otevrena-data/468E977C-DE78-480D-B3D9-43A19BF1CD77"
+    URL_jgw = "https://opendata.iprpraha.cz/CUR/ORT/ORT/S_JTSK/{identifier}.jgw"
+    URL_jpg = "https://opendata.iprpraha.cz/CUR/ORT/ORT/S_JTSK/{identifier}.jpg"
+
+    grid_shp = 'grid.shp'
+
+    get_polygons_from_url(URL, URL_jgw, grid_shp)
+
+
